f80dd254b2ed5920b0180dd792eb5fde502cc3cf
David Jenkins <djenk008@odu.edu>
2015-06-16 21:52:21 -0400

    push 404-error.php
    push 404-error.php

---
+++ 404-error.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    <!--Header Short -->    
+    <?php include('php/header_nav.php');?>
+    
+    <!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="fonts/icons/flaticon.css">
+
+    <!-- Nivo Lightbox
+    ================================================== -->
+    <link rel="stylesheet" href="css/nivo-lightbox.css" >
+    <link rel="stylesheet" href="css/nivo_lightbox_themes/default/default.css">
+
+    <!-- Slider
+    ================================================== -->
+    <link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+    <link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+    <!-- Stylesheet
+    ================================================== -->
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+    <!-- Google Font
+    ================================================== -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Muli:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat+Subrayada:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,100,800,900,400,200,300' rel='stylesheet' type='text/css'>
+
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+  </head>
+  <body>
+
+	<?php include('php/header_main_nav.php');?>
+    
+    <!-- Page Header
+    ========================-->
+    <div id="en-header">
+        <div class="container">
+            <h2 class="pull-left">Error Page</h2>
+            <ol class="breadcrumb pull-right">
+              <li><a href="index.html">Home</a></li>
+              <li class="active">Error Page</li>
+            </ol>
+        </div>
+    </div>
+
+    <!-- Page Content
+    ========================-->
+    <div id="en-content">
+        <div class="container">
+            <div class="col-sm-4 col-md-4">
+                <div class="error">
+                    <i class="flaticon-constructor7"></i>
+                </div>
+            </div>
+            <div class="col-sm-8 col-md-6">
+                <div class="error-text">
+                    <h1>404</h1>
+                    <h2>OOPS! PAGE NOT FOUND</h2>
+                    <br>
+                    <p>We try everything to look for the page you are searching but, sorry, but we can’t seem to find the page you are looking for.</p>
+                    <br>
+                    <a class="read-more" href="index.html">Go Back to Homepage <span class="fa fa-chevron-circle-right"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- CTA
+    ========================-->
+    <div class="en-cta">
+        <div class="overlay color">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <h2>Looking for the Best Engineering Solution for your Project?</h2>
+                    </div>
+                    <div class="col-md-3">
+                        <a class="btn btn-default en-btn light" href="contact.php" role="button">Get Started Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer Area -->
+	<?php include('php/footer.php');?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+    <script type="text/javascript" src="js/owl.carousel.js"></script><!-- Owl Carousel Plugin -->
+    <script type="text/javascript" src="js/nivo-lightbox.min.js"></script><!-- LightBox Nivo -->
+
+    <!-- Portfolio Filter -->
+    <script type="text/javascript" src="js/jquery.isotope.js"></script>
+    <script type="text/javascript" src="js/imagesloaded.js"></script>
+    
+    <!-- Javascripts
+    ================================================== -->
+    <script type="text/javascript" src="js/main.js"></script>
+
+    </body>
+</html>
